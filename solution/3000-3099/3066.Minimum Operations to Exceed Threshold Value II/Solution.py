1ed5db51317755f4c873616fc9af3f595fc704c8
sathya2809 <144447233+sathya2809@users.noreply.github.com>
2024-11-05 09:43:00 +0530

    Delete solution directory

--- solution/3000-3099/3066.Minimum Operations to Exceed Threshold Value II/Solution.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def minOperations(self, nums: List[int], k: int) -> int:
-        heapify(nums)
-        ans = 0
-        while len(nums) > 1 and nums[0] < k:
-            x, y = heappop(nums), heappop(nums)
-            heappush(nums, min(x, y) * 2 + max(x, y))
-            ans += 1
-        return ans
